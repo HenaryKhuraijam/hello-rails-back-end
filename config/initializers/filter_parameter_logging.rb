9825822930e35b51de577adc2d1a87b48ac27d57
HenaryKhuraijam <henarykhu123@gmail.com>
2024-01-29 20:37:20 +0530

    Fix Linter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# --
+
 # Be sure to restart your server when you modify this file.
 
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
